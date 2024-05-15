17460f8eaaf51eb10a93dfacb9317397e607d539
jonyLon <masterlordgoker@gmail.com>
2024-05-15 20:06:33 +0300

    Add project files.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace StreetLookupClient
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnGetStreetsClick(object sender, RoutedEventArgs e)
+        {
+            resultTextBlock.Text = await GetStreetsAsync(zipCodeTextBox.Text);
+        }
+
+        private Task<string> GetStreetsAsync(string zipCode)
+        {
+            return Task.Run(() =>
+            {
+                using (var client = new TcpClient("localhost", 8000))
+                {
+                    var stream = client.GetStream();
+                    byte[] data = Encoding.ASCII.GetBytes(zipCode);
+
+                    stream.Write(data, 0, data.Length);
+
+                    byte[] response = new byte[1024];
+                    int bytesRead = stream.Read(response, 0, response.Length);
+                    return Encoding.ASCII.GetString(response, 0, bytesRead);
+                }
+            });
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Xml.Linq;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var listener = new TcpListener(IPAddress.Loopback, 8000);
+        listener.Start();
+        Console.WriteLine("Server is running...");
+
+        while (true)
+        {
+            var client = listener.AcceptTcpClient();
+            Console.WriteLine("Client connected.");
+            var stream = client.GetStream();
+
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string zipCode = System.Text.Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+            string response = GetStreetsByZipCode(zipCode);
+            byte[] data = System.Text.Encoding.ASCII.GetBytes(response);
+
+            stream.Write(data, 0, data.Length);
+            client.Close();
+        }
+    }
+
+    static string GetStreetsByZipCode(string zipCode)
+    {
+        var doc = XDocument.Load(@"C:\Users\user\Desktop\ШАГ\current\homework\NP_sync_sockets-1\Server\Streets.xml");
+        var streets = doc.Descendants("Street")
+            .Where(st => st.Attribute("ZipCode").Value == zipCode)
+            .Select(st => st.Value)
+            .ToArray();
+
+        return string.Join(", ", streets);
+    }
+}
+
